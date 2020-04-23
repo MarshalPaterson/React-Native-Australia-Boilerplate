761d6ee1975837b322b55027386b703d2af2e305
Marshal Paterson <MARSHAL.PATERSON@star.com.au>
2020-04-23 15:32:21 +1000

    Adding Unit Tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
+  preset: "react-native",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  //collectCoverage: true,
+  //coverageDirectory: "__coverage__",
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testRegex: "./__tests__/[^setup].*.js$",
+  transformIgnorePatterns: ["/node_modules/(?!react-native)/.+"],
+  setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
+};
