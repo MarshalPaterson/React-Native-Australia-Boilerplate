d3e17a1b9e516d3a599faabdb7d0fbd7721b4794
Marshal Paterson <MARSHAL.PATERSON@star.com.au>
2020-04-27 11:14:48 +1000

    Fixing with lint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { AppRegistry, YellowBox } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./app.json";
 
-YellowBox.ignoreWarnings(['Warning: ...']);
+YellowBox.ignoreWarnings(["Warning: ..."]);
 
 console.disableYellowBox = true;
 
